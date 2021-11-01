170e0003dd42984b9de292138574912d64646ec5
manak1 <mana23happy@hotmail.co.jp>
2021-11-01 10:47:59 +0900

    :tada: chore: プロジェクトの作成

---
+++ README.md
@@ -0,0 +1,8 @@
+### 自分用 Next.js テンプレート
+
+個人開発で利用する Next.js テンプレートリポジトリです。  
+hygen でコンポーネント/テスト/storybook/スタイルファイルを一気に生成できる所がお気に入りです:penguin:
+
+```
+$npx hygen component new
+```
---
+++ next.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    eslint: {
+      dirs: ['src/components', 'src/hooks', 'src/pages'],
+    },
+  },
+  webpack(config) {
+    config.resolve.alias['~'] = path.join(__dirname, 'src')
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
